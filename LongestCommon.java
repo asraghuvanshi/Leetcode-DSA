1c68ad6fab3ad6a85ad7469924a7c52111a86938
asraghuvanshi <asraghuvanshi52@.com>
2023-01-31 01:29:34 +0530

    Longest common string

---
+++ LongestCommon.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LongestCommon {
+    public static void main(String[] args) {
+        // List<Integer> list = List.of(1, 2 ,3 ,4 , 5, 6);
+
+        int i = 0 , j = 0 ;
+        List<String> arraylist = Arrays.asList("python" , "tytho" , "pyth" , "py" , "p");
+        String prefix = arraylist.get(0);
+
+        for(String str : arraylist) {
+            while(i< prefix.length() && j<str.length()){
+                if(arraylist.get(0).charAt(i) != str.charAt(j)){
+                    System.out.println(i+" - "+j);
+
+                    break;
+                }
+                i++; 
+                j++;
+               
+            }
+        }
+        System.out.print(prefix.substring(0, i));
+    }
+}
