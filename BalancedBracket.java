61da995e7f7760c3ab2d5c9229c9bce6ac18392e
asraghuvanshi <asraghuvanshi52gmail.com>
2023-02-22 23:29:37 +0530

    updates

--- BalancedBracket.java
+++ BalancedBracket.java
@@ -4,10 +4,14 @@ public static void main(String[] args)
     {
         String expr = "([{}])";
  
-        // Function call
         if (areBracketsBalanced(expr))
             System.out.println("Balanced ");
         else
             System.out.println("Not Balanced ");
     }
+
+private static boolean areBracketsBalanced(String expr) {
+    System.out.println(expr);
+    return false;
+}
 }
