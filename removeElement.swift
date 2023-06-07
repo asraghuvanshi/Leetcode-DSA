654a81ad1b45e51e6b4e97ab35eb7c48f9b515d7
A.S Raghuvanshi <110293033+asraghuvanshi@users.noreply.github.com>
2023-06-07 23:42:39 +0530

    Update removeElement.swift

--- removeElement.swift
+++ removeElement.swift
@@ -1,3 +1,28 @@
+/*
+*
+*     Leetcode : Easy
+*     Date: 07/06/2023
+*/
+//  Solution 1
+func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+    var i = 0
+    var j = 0
+    
+    while i < nums.count {
+        if nums[i] == val {
+            i += 1
+        } else {
+            nums[j] = nums[i]
+            i += 1
+            j += 1
+        }
+    }
+    
+    return j
+}
+
+
+// Solution 2
 class Solution {
  func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
    nums = nums.filter { $0 != val }
