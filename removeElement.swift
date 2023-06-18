2f3e41624cb99ec89674065866bde6ff487d81a4
A.S Raghuvanshi <110293033+asraghuvanshi@users.noreply.github.com>
2023-06-18 22:34:17 +0530

    Update removeElement.swift

--- removeElement.swift
+++ removeElement.swift
@@ -4,22 +4,31 @@
 *     Date: 07/06/2023
 */
 //  Solution 1
-func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-    var i = 0
-    var j = 0
-    
-    while i < nums.count {
-        if nums[i] == val {
-            i += 1
-        } else {
-            nums[j] = nums[i]
-            i += 1
-            j += 1
+class Solution{
+
+
+    func removeElement(_ nums: inout [Int] , _ val: Int) ->  Int{
+        var i = 0 , j = 0;
+        while( i < nums.count){
+            if nums[i] == val{
+                i += 1
+            }
+            else {
+                nums[j] = nums[i]
+                i += 1
+                j += 1
+            }
         }
+        return j
     }
-    
-    return j
 }
+
+
+var arr = [10, 11, 9, 10, 11, 1, 2, 3, 4, 5]
+var val = 10
+let obj = Solution()
+let result = obj.removeElement(&arr, val)
+print(result)
 
 
 // Solution 2
